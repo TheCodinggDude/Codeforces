31baabe0c7c070016c7cfdb23150f6f6063feb84
Daydreamer008 <58206481+Daydreamer008@users.noreply.github.com>
2020-12-17 19:35:54 +0530

    Add files via upload

---
+++ A. Beautiful Matrix.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    int x = 0;
+    for (int i = 1; i <= 5; ++i)
+    {
+        for (int j = 1; j <= 5; ++j)
+        {
+            cin >> x;
+            if (x == 1)
+            {
+                cout << abs(i - 3) + abs(j - 3) << endl;
+            }
+        }
+    }
+	return 0;
+}
