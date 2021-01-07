da4e336ea32081469ad8e300763a5c2cd863bcbe
Daydreamer008 <58206481+Daydreamer008@users.noreply.github.com>
2021-01-07 18:52:22 +0530

    Add files via upload

---
+++ A. Gravity Flip.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define endl "\n"
+ 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+ 
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+ 
+#define mod 1000000007
+#define inf 1000000000000000001;
+ 
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+ 
+#define eb emplace_back
+#define f first
+#define s second
+ 
+void solve() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i =0 ; i<n ; ++i) {
+        cin>>a[i];
+    }
+    sort(a, a+n);
+    for(int i =0 ; i<n ; ++i) {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+}
+ 
+int main() {
+    // cin.ignore(); must be there when using getline(cin, s)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    solve();
+    return 0;
+}
