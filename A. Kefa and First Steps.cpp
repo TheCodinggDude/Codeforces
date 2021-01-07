fb51db07d2d3b62f285a0e3ec652dd01417367d5
Daydreamer008 <58206481+Daydreamer008@users.noreply.github.com>
2021-01-07 18:48:52 +0530

    Add files via upload

---
+++ A. Kefa and First Steps.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define endl "\n"
+ 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+ 
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+ 
+#define mod 1000000007
+#define inf 1000000000000000001;
+ 
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+ 
+#define eb emplace_back
+#define f first
+#define s second
+ 
+void solve() {
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i =0 ; i<n ; ++i) {
+        cin>>a[i];
+    }
+    int c =1 , maxc = 1;
+    for(int i =1 ; i<n ; ++i) {
+        if(a[i-1] <= a[i]) {
+            c++;
+            maxc = max(maxc , c);
+        }
+        else {
+            c = 1;
+        }
+    }
+    cout<<maxc;
+}
+ 
+int main() {
+    // cin.ignore(); must be there when using getline(cin, s)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+    solve();
+    return 0;
+}
