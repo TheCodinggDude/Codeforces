b9259e71a2862d6c2f756c36628b72b68a9bfcf8
Daydreamer008 <58206481+Daydreamer008@users.noreply.github.com>
2021-01-13 13:38:39 +0530

    Add files via upload

---
+++ A. New Year and the Christmas Ornament.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define endl "\n"
+ 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+ 
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+ 
+#define mod 1000000007
+#define inf 1000000000000000001;
+ 
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define asc(c) c.begin(),c.end()
+#define des(c) c.begin(),c.end(),greater<int>()
+
+#define pb push_back
+
+#define eb emplace_back
+#define f first
+#define s second
+ 
+
+void solve() {
+    int yellow , blue , red , sum=0;
+    cin >> yellow >> blue >> red;
+    for(int i =1 ; i<=yellow ; i++) {
+        if(blue >= i+1 && red >= i+2) {
+            sum = 3*i +3;
+        }
+    }
+    cout<<sum<<"\n";
+}
+ 
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
