2b34a8ddcd27f216695e7dae0351709daf29c689
Daydreamer008 <58206481+Daydreamer008@users.noreply.github.com>
2021-01-19 15:05:18 +0530

    Add files via upload

---
+++ 189A. Cut Ribbon.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define endl "\n"
+ 
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+ 
+#define pii pair<int,int>
+#define pll pair<long long, long long>
+ 
+#define mod 1000000007
+#define inf 1000000000000000001;
+ 
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define asc(c) c.begin(),c.end()
+#define des(c) c.begin(),c.end(),greater<int>()
+ 
+#define pb push_back
+ 
+#define eb emplace_back
+#define f first
+#define s second
+ 
+ 
+void solve() {
+	int n, a, b, c, pieces = 0;
+	cin >> n >> a >> b >> c;
+ 
+	for (int i = 0; i*a <= n; ++i)
+	{
+		for (int j = 0; i*a + j*b <= n; ++j)
+		{
+			int k = (n - i*a - j*b)/c;
+			if (i*a + j*b + k*c == n)
+			pieces = max(i + j + k, pieces);
+		
+ 
+		}
+	}
+	cout << pieces <<"\n";
+ 
+}
+ 
+ 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+    return 0;
+}
